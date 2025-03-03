3cb788f51849d4f2759442568d99f6cf1d91c7a9
Yassine <ya.mendili@gmail.com>
2025-03-03 15:55:41 -0500

    fixed the media issue. now we're able to use media

--- Deliverable 3/Part1/src/main/java/module-info.java
+++ Deliverable 3/Part1/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.part1 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media; //added this line for media use
 
     opens controllers to javafx.fxml;
     exports app;
